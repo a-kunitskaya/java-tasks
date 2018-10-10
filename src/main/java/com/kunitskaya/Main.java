947e5b3e6e64ef485e09d74822897b6a9febcab1
Alexandra_Kunitskaya <alexandra_kunitskaya@epam.com>
2018-10-10 11:54:29 +0300

    Add the initial implementation of houshold appliances, add logging

---
+++ src/main/java/com/kunitskaya/Main.java
@@ -0,0 +1,15 @@
+package com.kunitskaya;
+
+import com.kunitskaya.entities.Fridge;
+
+/**
+ * Created by Alexandra Kunitskaya
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Fridge fridge = new Fridge();
+		fridge.plugIn();
+	}
+}
