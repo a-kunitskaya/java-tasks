54d44474f061e5a3e175cf175ec472fc96b0eb29
Alexandra_Kunitskaya <alexandra_kunitskaya@epam.com>
2018-10-11 11:44:05 +0300

    Revert "Add sorting and finding"
    This reverts commit f2d429fcc40085d6d2da93aa7b0e482e14154b46.

--- src/main/java/com/kunitskaya/service/Sortable.java
+++
@@ -1,9 +0,0 @@
-package com.kunitskaya.service;
-
-import com.kunitskaya.entities.HouseholdAppliance;
-
-import java.util.List;
-
-public interface Sortable {
-    void sortByPowerConsumption(List<HouseholdAppliance> appliances);
-}
