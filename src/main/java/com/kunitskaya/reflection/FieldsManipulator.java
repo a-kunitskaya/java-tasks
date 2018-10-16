0909af676d2fca0ae854b2945f613aa56017c60e
Alexandra_Kunitskaya <alexandra_kunitskaya@epam.com>
2018-10-16 09:52:43 +0300

    Add reflection for fields

--- src/main/java/com/kunitskaya/reflection/FieldsManipulator.java
+++ src/main/java/com/kunitskaya/reflection/FieldsManipulator.java
@@ -1,12 +1,48 @@
 package com.kunitskaya.reflection;
 
+import com.kunitskaya.domain.HouseholdAppliance;
+import com.kunitskaya.logging.ProjectLogger;
+
+import java.lang.reflect.Field;
+
 /**
  * Manipulates class fields with reflection
  */
 public class FieldsManipulator {
 
-// fills in fields of the class with values ?
-    public static void fillInFields(Class<?> clazz){
+    // fills in fields of the class with values ?
 
+    //TODO: join 2 arrays!
+
+
+    public static void fillInFieldsWithReflection(HouseholdAppliance instanceToGetValuesFrom, HouseholdAppliance instanceToFill) {
+        Class<? extends HouseholdAppliance> modelClazz = instanceToGetValuesFrom.getClass();
+        Class<? extends HouseholdAppliance> clazz = instanceToFill.getClass();
+
+        if (modelClazz.getSimpleName().equals(clazz.getSimpleName())) {
+            int arraySize = clazz.getDeclaredFields().length + clazz.getSuperclass().getDeclaredFields().length;
+            Field[] fields = new Field[arraySize];
+            Field[] superFields = clazz.getSuperclass().getDeclaredFields();
+            Field[] childFields = clazz.getDeclaredFields();
+
+            for(int i = 0; i < arraySize; i++) {
+                fields[i] = superFields[i];
+            }
+
+
+
+            for (Field field : fields){
+                field.setAccessible(true);
+                try {
+                    field.set(instanceToFill, field.get(instanceToGetValuesFrom));
+                } catch (IllegalAccessException e) {
+                    ProjectLogger.LOGGER.info("Could not get access to field " + field.getName());
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            String message = "Instance to get values from (%s) is different from instance to fill in (%s)";
+            throw new IllegalArgumentException(String.format(message, modelClazz, clazz));
+        }
     }
 }
--- src/main/java/com/kunitskaya/reflection/ReflectionExecutor.java
+++ src/main/java/com/kunitskaya/reflection/ReflectionExecutor.java
@@ -3,6 +3,7 @@ package com.kunitskaya.reflection;
 import com.kunitskaya.domain.Fridge;
 import com.kunitskaya.domain.HouseholdAppliance;
 import com.kunitskaya.domain.Kettle;
+import com.kunitskaya.domain.data.HomeLocation;
 
 /**
  * Shows Reflection API usage
@@ -16,5 +17,9 @@ public class ReflectionExecutor {
         //M2 - Task 2 - Reflection API usage
         HouseholdAppliance kettle = ClassInstantiator.instantiate(Kettle.class, 200);
         HouseholdAppliance fridge = ClassInstantiator.instantiate(Fridge.class);
+
+        HouseholdAppliance modelFrigge = new Fridge(100, "red", HomeLocation.LIVING_ROOM, -4);
+
+        FieldsManipulator.fillInFieldsWithReflection(modelFrigge, fridge);
     }
 }
