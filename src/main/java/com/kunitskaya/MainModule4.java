f59a1051ef0d3c657d6e196968fc0794c3b45fd3
Alexandra <Alexandra.kunitskaya@gmail.com>
2018-11-06 15:23:13 +0300

    Add some SpEL

--- src/main/java/com/kunitskaya/MainModule4.java
+++ src/main/java/com/kunitskaya/MainModule4.java
@@ -7,6 +7,7 @@ import com.kunitskaya.service.module4.EmployeeService;
 import com.kunitskaya.service.module4.PositionService;
 import com.kunitskaya.service.module4.SalaryService;
 import com.kunitskaya.service.module4.config.AppContext;
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -20,6 +21,7 @@ public class MainModule4 {
         ApplicationContext appContextNoAutowiring = new ClassPathXmlApplicationContext("Beans.xml");
 
         Salary devSalary = (Salary) appContextNoAutowiring.getBean("dev_salary");
+
         Position devPosition = (Position) appContextNoAutowiring.getBean("dev_position");
         Employee javaDeveloper = (Employee) appContextNoAutowiring.getBean("java_dev_employee");
         Employee iosDeveloper = (Employee) appContextNoAutowiring.getBean("ios_dev_employee");
@@ -29,6 +31,8 @@ public class MainModule4 {
 
         EmployeeService employeeServiceXml = (EmployeeService) appContextAutowiring.getBean("employee_service");
         SalaryService salaryServiceXml = (SalaryService) appContextAutowiring.getBean("salary_service");
+        System.out.println(salaryServiceXml);
+
 
 
         //Task 2: autowiring with annotations
