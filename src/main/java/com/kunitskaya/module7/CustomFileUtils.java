81aa33d5dd4359d3ee4807e5ee9d435322cc8a06
Alexandra_Kunitskaya <alexandra_kunitskaya@epam.com>
2018-11-21 19:03:52 +0300

    Add the initial set up for M7

---
+++ src/main/java/com/kunitskaya/module7/CustomFileUtils.java
@@ -0,0 +1,11 @@
+package com.kunitskaya.module7;
+
+import java.io.File;
+
+public class CustomFileUtils {
+    public static void moveFile(String fileToMove, String to) {
+        File sourceFilePath = new File(fileToMove);
+        File targetDir = new File(to);
+
+    }
+}
