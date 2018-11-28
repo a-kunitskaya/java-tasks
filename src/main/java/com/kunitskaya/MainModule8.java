a02f00d90cd7f8e0a5b9449f48a058615c9ff53a
Alexandra_Kunitskaya <alexandra_kunitskaya@epam.com>
2018-11-28 18:45:00 +0300

    Add dependencies for JDBC

---
+++ src/main/java/com/kunitskaya/MainModule8.java
@@ -0,0 +1,8 @@
+package com.kunitskaya;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MainModule8 {
+    Connection connection = DriverManager.getConnection("", "", "");
+}
---
+++ src/main/java/com/kunitskaya/module8/ConfigProvider.java
@@ -0,0 +1,4 @@
+package com.kunitskaya.module8;
+
+public class ConfigProvider {
+}
