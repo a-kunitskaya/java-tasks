ac7fdcf09ae648457617d7db20f47b71297dfa12
Alexandra <Alexandra.kunitskaya@gmail.com>
2018-11-29 08:05:43 +0300

    Add config provider

--- src/main/java/com/kunitskaya/MainModule8.java
+++ src/main/java/com/kunitskaya/MainModule8.java
@@ -1,8 +1,28 @@
 package com.kunitskaya;
 
+import com.kunitskaya.module8.ConfigProvider;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MainModule8 {
-    Connection connection = DriverManager.getConnection("", "", "");
+    private static ConfigProvider configProvider = new ConfigProvider();
+
+    private static String db = configProvider.getDBUrl();
+    private static String username = configProvider.getDBUsername();
+    private static String password = configProvider.getDBPassword();
+
+    public static void main(String[] args) {
+
+
+        try {
+            Connection connection = DriverManager.getConnection(db, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
--- src/main/java/com/kunitskaya/module8/ConfigProvider.java
+++ src/main/java/com/kunitskaya/module8/ConfigProvider.java
@@ -1,4 +1,40 @@
 package com.kunitskaya.module8;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static com.kunitskaya.logging.ProjectLogger.LOGGER;
+
 public class ConfigProvider {
+    private static final String PROPERTIES_PATH = "src/main/resources/module8/jdbc.properties";
+
+    private Properties properties = new Properties();
+
+    public ConfigProvider() {
+        loadProperties();
+    }
+
+    private void loadProperties() {
+        try (InputStream inputStream = new FileInputStream(PROPERTIES_PATH)) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        LOGGER.info("Successfully loaded properties from: " + PROPERTIES_PATH);
+    }
+
+    public String getDBUsername() {
+        return properties.getProperty("jdbc.username");
+    }
+
+    public String getDBPassword() {
+        return properties.getProperty("jdbc.password");
+    }
+
+    public String getDBUrl() {
+        return properties.getProperty("jdbc.url");
+    }
 }
