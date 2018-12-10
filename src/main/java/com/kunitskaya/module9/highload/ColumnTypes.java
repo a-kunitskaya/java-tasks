5ab8a4444de34b417c0f126e5def4553f27df262
Alexandra <Alexandra.kunitskaya@gmail.com>
2018-12-10 07:57:07 +0300

    Add random tables creation initial implementation

---
+++ src/main/java/com/kunitskaya/module9/highload/ColumnTypes.java
@@ -0,0 +1,7 @@
+package com.kunitskaya.module9.highload;
+
+public enum ColumnTypes {
+    VARCHAR,
+    INTEGER,
+    BOOLEAN
+}
---
+++ src/main/java/com/kunitskaya/module9/highload/HighloadDatabaceOperations.java
@@ -0,0 +1,4 @@
+package com.kunitskaya.module9.highload;
+
+public class HighloadDatabaceOperations {
+}
